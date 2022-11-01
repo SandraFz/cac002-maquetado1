40f536fbb1beedd9666ab30dade7c01e794a1ff0
Sandra <sandranfernandez@gmail.com>
2022-11-01 20:21:15 -0300

    DHice conversiones.

--- pages/comprar_tickets.js
+++ pages/comprar_tickets.js
@@ -1,21 +1,30 @@
 //CONSTANTES
 
-const estudianteDesc = document.getElementById("estudiante").innerText;
-const traineeDesc = document.getElementById("trainee").innerText;
-const juniorDesc = document.getElementById("junior").innerText;
-const valor = document.getElementById("valor").innerText;
+let estudianteDesc = document.getElementById("estudiante").innerText;
+console.log(estudianteDesc)
+let traineeDesc = document.getElementById("trainee").innerText;
+console.log(traineeDesc)
+let juniorDesc = document.getElementById("junior").innerText;
+console.log(juniorDesc)
+const valor = Number(document.getElementById("valor").innerText);
+console.log(typeof valor)
 //VARIABLES
 
 let nombre = document.getElementById("name").innerText;
 let lastName = document.getElementById("lastname").innerText;
-let cantidad = document.getElementById("cantidad").value;
+let cantidad = Number(document.getElementById("cantidad").innerText);
+console.log("Cantidad fuera del método: " + cantidad + typeof cantidad)
 let resumen = document.getElementById("resumen"); 
 
 //CÁLCULO DEL MONTO TOTAL
-
+  
 function calculoTotal(){
+    let totalBruto = valor * Number(cantidad);
 
-    let totalBruto = valor * cantidad;
+    console.log("Esta es el valor dentro del método: " + valor + typeof valor);
+    console.log("Esta es la cantidad dentro del método: " + cantidad + typeof cantidad);
+    console.log("Esta es el totalBruto dentro del método: " + totalBruto + typeof totalBruto);
+
     let categoria = document.getElementById("categoria").value;
     let totalPorCateg;
     
@@ -24,6 +33,7 @@ function calculoTotal(){
         case "estudiante":
            
             totalPorCateg = totalBruto - ((parseInt(estudianteDesc) / 100) * totalBruto)
+            console.log("Este es el total Bruto de estudiante: " + totalBruto);
             break;
     
         case "trainee":
@@ -35,14 +45,13 @@ function calculoTotal(){
             break;
 
         default:
-            totalPorCateg = valor;
+            totalPorCateg = totalBruto;
+            break;
     };
-/*
-    let totalInput = document.getElementById("total");
-    totalInput.value = "Total a pagar: $" + totalPorCateg;
-*/
 
-    return totalPorCateg;
+    let totalInput = document.getElementById("total");
+    totalInput.innerText = totalPorCateg;
+    totalInput.innerHTML = totalPorCateg;
 }
 
 //ENVÍO EL VALOR AL INPUT "TOTAL A PAGAR"
